06b5689bee8bb7775995aaa5b1aa87d0a8399ff0
Wilson Lau <wilsonlau@ResNet-23-187.resnet.ucsb.edu>
2022-10-02 14:52:11 -0700

    fixed unsigned long thing

--- welcome/code/olleh.cpp
+++ welcome/code/olleh.cpp
@@ -5,12 +5,12 @@ using namespace std;
 
 int main ()
 {
-  int i=0;
+  unsigned long i=0;
   string str;
   string finalString = "";
   string temp = "";
   
-  cout << "Input: ";
+  cout << "";
   getline(cin, str);
   
   while (str[i])
