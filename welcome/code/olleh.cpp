5675d1202ff5b580d893e01fbd25de37253e27d0
Wilson Lau <wilsonlau@ResNet-23-187.resnet.ucsb.edu>
2022-10-02 16:41:24 -0700

    hope this works man

--- welcome/code/olleh.cpp
+++ welcome/code/olleh.cpp
@@ -15,21 +15,32 @@ int main ()
   
   while (str[i])
   {
-    temp += str[i];
-    if (ispunct(str[i]) || isspace(str[i]) || i == str.size() -1 )
+    temp += str[i];        
+    if (ispunct(str[i]) || isspace(str[i]) || i == str.size() - 1 ) 
     {
-        for(int f = temp.size() - 1; f >= 0; f--)                      
+        for(int f = temp.size() - 1; f >= 0; --f)                   
         {                                          
-            if (temp[f] != str[i])
+            if (!ispunct(temp[f]) && !isspace(temp[f]))   
             {
-            finalString += temp[f];
+            finalString += temp[f];                                                
             }
         }    
-        finalString += str[i];
-    temp = "";
+    if ((ispunct(str[i])) || (isspace(str[i])))
+    {
+        finalString += str[i];  
+    } 
+    temp = "";                                                          
     }
     i++;
   }
   cout << finalString;
   return 0;
 }
+
+/* aibohporypapoigruoimedoleta
+Q
+/
+	esaelP niatniaM	laicoS     ecnatsiD   
+ (emiT, uoht tsetapicitna ym daerd stiolpxe.)
+ tI tsoc $943.59, tub I thguob ti yawyna.
+*/
