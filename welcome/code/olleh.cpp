5bc9a26d1ed57f73fd1c1a8730c7bb66c3ad23a9
Wilson Lau <wilsonlau@ResNet-23-187.resnet.ucsb.edu>
2022-10-02 12:32:35 -0700

    welcome 1st submission

---
+++ welcome/code/olleh.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+  int i=0;
+  string str;
+  string finalString = "";
+  string temp = "";
+  
+  cout << "Type something: ";
+  getline(cin, str);
+  while (str[i])
+  {
+    if (ispunct(str[i]) || isspace(str[i]))
+    {
+        for(int f = temp.size() - 1; f >= 0 ; --f)                      
+        {                                          
+        finalString += temp[f];
+        }                     
+        finalString += str[i];                       
+        temp = "";
+    } else {
+        temp += str[i];
+    }
+    
+    i++;
+  }
+  
+  cout << finalString;
+  cout << "make test";
+  return 0;
+}
+
+
