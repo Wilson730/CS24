686e976a1f4d11eadcc8e5b9b9feb68928b7468e
Wilson Lau <wilsonlau@ResNet-23-187.resnet.ucsb.edu>
2022-10-08 16:20:00 -0700

    test

--- fibvec/FibVec.h
+++ fibvec/FibVec.h
@@ -6,15 +6,15 @@ using namespace std;
 
 class FibVec{
 private:
-    int cap;
-    int nrOfEl;
+    size_t cap;
+    size_t count;
     int *arr;
-
+    int x, y;
 public: 
 
     FibVec(){
-    int x = 1, y = 1;
-    nrOfEl = 0;
+    x = 1, y = 1;
+    count = 0;
     cap = 10;
     arr = new int[cap];
     }
@@ -23,6 +23,21 @@ public:
         delete[] arr;
     }
     
+    void resize(){
+        cap = x + y;
+        int *temparr = new int[cap];
+
+        for (size_t i = 0; i < count; i++)
+        {
+            temparr[i] = arr[i];
+        }
+
+        delete[] arr;
+         
+        arr = temparr;
+
+        
+    }
     size_t capacity() const;
     size_t count() const;
     int insert(int value, size_t index);
@@ -31,4 +46,3 @@ public:
     int push(int value);
     int remove(size_t index);
 };
-
