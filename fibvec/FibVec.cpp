f72879dc0164ef9c1151223c6928f94dee35cb2b
Wilson Lau <wilsonlau@ResNet-23-187.resnet.ucsb.edu>
2022-10-09 20:29:51 -0700

    testing insert one

--- fibvec/FibVec.cpp
+++ fibvec/FibVec.cpp
@@ -73,25 +73,25 @@ arr = temparr;
 }
 //---------------------------------------------------------------- INSERT ----------------------------------------------------------------------
 void FibVec::insert(int value, size_t index){
-   /* if (index > nrOfEl || index > cap){ // index cannot be negative, skip, or over. 
+   /* if (index > nrOfEl || index > cap){ 
         throw std::out_of_range("invalid index");
-    } else if ((index == cap) && (cap == nrOfEl)) {// insert at cap 1 after the total size 
+    } else if ((index == cap) && (cap == nrOfEl)) {
         ++nrOfEl;
         expand(); 
         arr[index] = value;
-    } else if(index < nrOfEl){                // insert in the middle
-        if (++nrOfEl > cap){                 // expands if adding one element will make it go over capacity 
+    } else if(index < nrOfEl){                
+        if (++nrOfEl > cap){                 
             --nrOfEl;       
             expand();
             ++nrOfEl;
         }
-        for(size_t i = index; i < nrOfEl; i++){   // moves values over 
+        for(size_t i = index; i < nrOfEl; i++){  
             arr[i + 1] = arr[i];
         }
-        arr[index] = value; */
-   // } else {
         arr[index] = value;
- //   }
+    } else { */
+        arr[index] = value;
+   // }
 }
 //----------------------------------------------------------------- PUSH -----------------------------------------------------------------------
 void FibVec::push(int value){
