63cc4a17459c982b25587a6dcbfc9315514c8ccc
Wilson Lau <wilsonlau@ResNet-23-187.resnet.ucsb.edu>
2022-10-10 15:54:01 -0700

    i put index int he wrong scope...

--- fibvec/FibVec.cpp
+++ fibvec/FibVec.cpp
@@ -94,8 +94,9 @@ void FibVec::insert(int value, size_t index){
         for(size_t i = nrOfEl - 2; i >= index; i--){            // don't worry about the end bc you've already expanded. 
             arr[i+1] = arr[i];
         }
-        }
         arr[index] = value;
+        }
+     
     } else { 
         arr[index] = value;
         ++nrOfEl;
