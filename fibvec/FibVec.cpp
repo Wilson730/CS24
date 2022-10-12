4414c7444b1d8dc784ec62ee7d6826292c5ea10a
Wilson Lau <wilsonlau@ResNet-23-187.resnet.ucsb.edu>
2022-10-11 21:18:39 -0700

    dunno

--- fibvec/FibVec.cpp
+++ fibvec/FibVec.cpp
@@ -129,16 +129,17 @@ int FibVec::remove(size_t index){
     if (index > nrOfEl || index > cap || ((index >= nrOfEl) && (nrOfEl != cap))){
         throw std::out_of_range("Index out of range.");
     } else {
-        --nrOfEl;
         int temp = arr[index]; 
         
         for(size_t i = index;  index < nrOfEl - index; i++){            
             arr[i] = arr[i+1];
-        }
+        } 
+        --nrOfEl;
         if ((nrOfEl < cap - x) || nrOfEl < cap - y){
         shrink();
         }
-       
+              
+
         return temp;
     }
     
