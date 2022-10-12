ed95d73e46f1aa439cdc0870ca7973a93c73cc2d
Wilson Lau <wilsonlau@ResNet-23-187.resnet.ucsb.edu>
2022-10-12 14:18:47 -0700

    switch positions?

--- fibvec/FibVec.cpp
+++ fibvec/FibVec.cpp
@@ -131,16 +131,16 @@ int FibVec::remove(size_t index){
         throw std::out_of_range("Index out of range.");
     } else {
         int temp = arr[index]; 
-         --nrOfEl;
-        if ((nrOfEl - index) > index){
-        
-        for(size_t i = index;  i < nrOfEl - index; ++i){        
-            arr[i] = arr[i+1];
-        } 
-        }
+         --nrOfEl; 
+         
         if ((nrOfEl < cap - x) || nrOfEl < cap - y){
         shrink();
         }
+        for(size_t i = index;  i < nrOfEl - index; ++i){        
+            arr[i] = arr[i+1];
+        } 
+        
+       
               
 
         return temp;
