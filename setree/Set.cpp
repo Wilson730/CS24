04877647c48b388e069e021dd3fbfd2c42547ec4
Wilson Lau <wilsonlau@ResNet-23-22.resnet.ucsb.edu>
2022-10-23 14:00:20 -0700

    dsf

--- setree/Set.cpp
+++ setree/Set.cpp
@@ -49,7 +49,6 @@ size_t Set::insert(const std::string&value){
     newNode->left = NULL;       // random node in mem containing value initialized
     newNode->right = NULL;
     Node* currNode = mRoot;
-
     if (mRoot == NULL){
         mRoot = newNode;
         mRoot->left = NULL;    // empty list? initialize set with a new head.
@@ -64,9 +63,10 @@ size_t Set::insert(const std::string&value){
         }
     } else {
         while (currNode != NULL){
-            if (currNode->left != NULL && value < currNode->left->data){
+            while (currNode->left != NULL && value < currNode->left->data){
             currNode = currNode->left;
-            } else if (currNode->right != NULL && value < currNode->right->data ){
+            } 
+            while (currNode->right != NULL && value < currNode->right->data ){
             currNode = currNode->right; 
             }
         }
@@ -89,7 +89,7 @@ void Set::print() const {
 size_t Set::remove (const std::string& value){
     size_t amountRemoved = 0;
     Node* currNode = mRoot;
-    Node* prevNode = NULL;
+  
     while (currNode->data < value){
 
     }
