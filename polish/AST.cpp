deac26fec4b488d3b29689d01e8542c190583d30
Wilson Lau <wilsonlau@ResNet-23-22.resnet.ucsb.edu>
2022-10-29 11:43:18 -0700

    sdf

--- polish/AST.cpp
+++ polish/AST.cpp
@@ -6,9 +6,10 @@
 using namespace std;
 
 AST* AST::parse(const std::string& expression) {
-
-    char token;
+   
+   
     std::istringstream mystream(expression);
+    char token;
     while (mystream>>token){
         switch(token){
             case '+':
