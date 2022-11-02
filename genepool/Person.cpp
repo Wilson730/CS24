be6db966775859ece7b9da4d793336a762cbd7bf
Wilson Lau <wilsonlau@ResNet-31-31.resnet.ucsb.edu>
2022-11-01 18:26:37 -0700

    sdf

--- genepool/Person.cpp
+++ genepool/Person.cpp
@@ -1,3 +1,32 @@
 #include "Person.h"
 
 // Person Member Functions
+Person::Person(){
+
+}
+
+Person::~Person(){
+
+
+}
+
+ std::set<Person*> Person::ancestors(PMod pmod = PMod::ANY){}
+  std::set<Person*> Person::aunts(PMod pmod = PMod::ANY, SMod smod = SMod::ANY){}
+  std::set<Person*> Person::brothers(PMod pmod = PMod::ANY, SMod smod = SMod::ANY){}
+  std::set<Person*> Person::children(){}
+  std::set<Person*> Person::cousins(PMod pmod = PMod::ANY, SMod smod = SMod::ANY){}
+  std::set<Person*> Person::daughters(){}
+  std::set<Person*> Person::descendants(){}
+  std::set<Person*> Person::grandchildren(){}
+  std::set<Person*> Person::granddaughters(){}
+  std::set<Person*> Person::grandfathers(PMod pmod = PMod::ANY){}
+  std::set<Person*> Person::grandmothers(PMod pmod = PMod::ANY){}
+  std::set<Person*> Person::grandparents(PMod pmod = PMod::ANY){}
+  std::set<Person*> Person::grandsons(){}
+  std::set<Person*> Person::nephews(PMod pmod = PMod::ANY, SMod smod = SMod::ANY){}
+  std::set<Person*> Person::nieces(PMod pmod = PMod::ANY, SMod smod = SMod::ANY){}
+  std::set<Person*> Person::parents(PMod pmod = PMod::ANY){}
+  std::set<Person*> Person::siblings(PMod pmod = PMod::ANY, SMod smod = SMod::ANY){}
+  std::set<Person*> Person::sisters(PMod pmod = PMod::ANY, SMod smod = SMod::ANY){}
+  std::set<Person*> Person::sons(){}
+  std::set<Person*> Person::uncles(PMod pmod = PMod::ANY, SMod smod = SMod::ANY){}
