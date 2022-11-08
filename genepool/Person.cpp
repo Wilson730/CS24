d2b5646b95243c172000a1c32b49becea2067606
Wilson Lau <wilsonlau@ResNet-31-31.resnet.ucsb.edu>
2022-11-07 16:20:29 -0800

    parents

--- genepool/Person.cpp
+++ genepool/Person.cpp
@@ -144,10 +144,10 @@ Person::Person(string n, Gender g, Person* m, Person* f){
   }
  
   std::set<Person*> Person::siblings(PMod pmod, SMod smod){
-    auto parnts = parents(pmod); // mum, pop, or both
+ // mum, pop, or both
     std::set<Person*> prntchilds;
-    for (auto itr1 = parnts.begin(); itr1 != parnts.end(); ++itr1){
-        prntchilds.merge((*itr1)->children());   // stores children of parent set
+    for (Person* parent : parents(pmod)){
+        prntchilds.merge(parent->children());   // stores children of parent set
     } //  how about duplicates...
 
     std::set<Person*> sibs;                // ^ access their children
