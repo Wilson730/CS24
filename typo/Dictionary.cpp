57f8dbfa91b5ba170065aebc2529518e7a73cda8
Wilson Lau <wilsonlau@ResNet-31-31.resnet.ucsb.edu>
2022-11-11 10:06:16 -0800

    sdf

--- typo/Dictionary.cpp
+++ typo/Dictionary.cpp
@@ -2,7 +2,7 @@
 
 Dictionary::Dictionary(std::istream& stream){
 
- };
+};
 
 Heap Dictionary::correct(const std::vector<Point>& points, size_t maxcount, float cutoff) const {
 
--- typo/Heap.cpp
+++ typo/Heap.cpp
@@ -1,3 +1,37 @@
 #include "Heap.h"
 
-Heap::Heap(size_t capacity){mCapacity = capacity;}
+
+Heap::Heap(size_t capacity){
+    mCapacity = capacity;
+};
+Heap::Heap(const Heap& other){
+
+};
+Heap::Heap(Heap&& other){
+
+};
+Heap::~Heap(){
+
+};
+
+size_t Heap::capacity() const{
+    return mCapacity;
+};
+size_t Heap::count() const{
+    return mCount;
+};
+const Heap::Entry& Heap::lookup(size_t index) const{
+
+};
+Heap::Entry Heap::pop(){
+
+};
+Heap::Entry Heap::pushpop(const std::string& value, float score){
+
+};
+void Heap::push(const std::string& value, float score){
+
+};
+const Heap::Entry& Heap::top() const {
+
+};
