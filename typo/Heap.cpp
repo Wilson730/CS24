62cb4d4af61653055ba954061ddcf7e9136e6bf9
Wilson Lau <wilsonlau@ResNet-31-31.resnet.ucsb.edu>
2022-11-11 10:20:09 -0800

    sdf

--- typo/Heap.cpp
+++ typo/Heap.cpp
@@ -3,6 +3,7 @@
 
 Heap::Heap(size_t capacity){
     mCapacity = capacity;
+    mData[mCapacity];
 };
 Heap::Heap(const Heap& other){
 
@@ -21,17 +22,17 @@ size_t Heap::count() const{
     return mCount;
 };
 const Heap::Entry& Heap::lookup(size_t index) const{
-
+    return mData[index];
 };
 Heap::Entry Heap::pop(){
-
+    return mData[0];
 };
 Heap::Entry Heap::pushpop(const std::string& value, float score){
-
+    return mData[0];
 };
 void Heap::push(const std::string& value, float score){
-
+    
 };
 const Heap::Entry& Heap::top() const {
-
+    return mData[0];
 };
