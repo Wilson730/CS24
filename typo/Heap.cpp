88f7d0a24dcb326713301e7aee46a3018cb7fcd6
Wilson Lau <wilsonlau@ResNet-31-31.resnet.ucsb.edu>
2022-11-12 12:05:39 -0800

    fixing seg fault

--- typo/Heap.cpp
+++ typo/Heap.cpp
@@ -4,6 +4,7 @@
 Heap::Heap(size_t capacity){
 
     mCapacity = capacity;
+    mCount = 0;
     mData = new Entry[mCapacity];
 };
 Heap::Heap(const Heap& other){
@@ -18,38 +19,70 @@ Heap::Heap(Heap&& other){
     this->mData = other.mData;
     delete [] other.mData;
 };
-
 Heap::~Heap(){
     delete [] mData;
 };
-
 size_t Heap::capacity() const{
     return mCapacity;
 };
 size_t Heap::count() const{
     return mCount;
 };
+
+
+//----------------------------------UNFINISHED-----------------------------------------------------
+
+
+
+
+
 const Heap::Entry& Heap::lookup(size_t index) const{
     return mData[index];
 };
+
 Heap::Entry Heap::pop(){
-    return mData[0];
+    Entry result;
+    if (mCount == 0){
+        throw std::underflow_error("No items");
+    }
+    for (size_t i = 0; i < mCount; i++){
+        if (i + 1 == mCount){
+            result = mData[i];
+
+            mCount -= 1;
+        }
+    }
+    return result;
 };
+
 Heap::Entry Heap::pushpop(const std::string& value, float score){
-    return mData[0];
+    Entry result = {};
+    return result;
 };
 void Heap::push(const std::string& value, float score){
+    /*
     if (mCount > mCapacity){
         throw std::overflow_error("overflow!");
     } else {
         Entry newEntry;
         newEntry.value = value;
         newEntry.score = score;
-        if (mCount > 0)   mData[mCount - 1] = newEntry; 
+        if (mCount > 0)   mData[mCount + 1] = newEntry; 
         else mData[0] = newEntry;  
         mCount += 1;
     }
+    */
 };
 const Heap::Entry& Heap::top() const {
-    return mData[0];
+    if (mCount == 0) throw std::underflow_error("Empty heap");
+    Entry& result = mData[0];
+    /*
+    if (mCount == 0) throw std::underflow_error("Empty");
+    for (size_t i = mCount; i > 0; i--){
+        if (mData[i - 1] < mData[i]){
+            result = mData[i-1];
+        }
+    }
+    */
+    return result;
 };
