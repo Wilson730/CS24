0f6df3a97ef8de101ad4bb70a3fd8f054d75c0d8
Wilson Lau <wilsonlau@ResNet-31-31.resnet.ucsb.edu>
2022-11-13 22:35:12 -0800

    sdf

--- typo/Dictionary.cpp
+++ typo/Dictionary.cpp
@@ -5,6 +5,7 @@ Dictionary::Dictionary(std::istream& stream){
 };
 
 Heap Dictionary::correct(const std::vector<Point>& points, size_t maxcount, float cutoff) const {
+    Heap* heap = new Heap(maxcount);
 
-
+    return *heap;
 };
