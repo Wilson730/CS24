a45b73bcc7312c5a0cd5df59a27ffc7cb33a3471
Wilson Lau <wilsonlau@ResNet-31-31.resnet.ucsb.edu>
2022-11-13 18:35:53 -0800

    dsf

--- typo/Heap.cpp
+++ typo/Heap.cpp
@@ -73,7 +73,7 @@ const Heap::Entry& Heap::lookup(size_t index) const{
 void percolateDown(Heap::Entry* mData, size_t i, size_t mCount){ 
     size_t leftChild = (i * 2) + 1;
     size_t rightChild = (i * 2) + 2;
-    if (i < mCount && (leftChild < mCount || rightChild < mCount)){
+    if (i < mCount){
     
     if (mData[i].score > mData[leftChild].score) { 
         Heap::Entry temp = mData[leftChild];  
