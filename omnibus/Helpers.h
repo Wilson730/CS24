e35f05430ac069ce561e9aa2293cb3606ca128e2
Wilson Lau <wilsonlau@ResNet-26-210.resnet.ucsb.edu>
2022-12-02 13:35:53 -0800

    sdf

--- omnibus/Helpers.h
+++ omnibus/Helpers.h
@@ -6,17 +6,20 @@
 // here's some space to do it in.
 using namespace std;
 
+struct Station;
+
 struct Neighbor{
   size_t distance;
-  string line; // (line used to get to *this* neighbor)
-  Station* nStation;
+  string line; 
+  Station* nStation; // pointer to neighbor in map. 
   bool train;
 };
 
 struct Station {
   string name;
-  string line; // (to get to *this* station)
-  vector<Neighbor*> neighbors;
+  string line; 
+
+  vector<Neighbor*> neighbors; 
 };
 
 
