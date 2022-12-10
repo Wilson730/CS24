2682da221fcf768ec67aa4ec444d90a5c0e49275
Wilson Lau <wilsonlau@Wilsons-MacBook-Air.local>
2022-12-09 21:00:51 -0800

    sdf

--- polish/AST.cpp
+++ polish/AST.cpp
@@ -95,12 +95,13 @@ AST* AST::parse(const std::string& expression) {
            }
            stack.push(new neg(child));
         } else {
+
             size_t index = 0;
             double value = stod(token, &index);
             try{
                 if(index != token.length()){
                     stack.clean();
-                    throw std::runtime_error("invalid");
+                    throw std::runtime_error("Invalid token: " + token);
                 }
                 stack.push(new numbers(value));
             }
