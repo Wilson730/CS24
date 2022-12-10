3f369d46b09a618aa0a691e08dcac13da898d80e
Wilson Lau <wilsonlau@Wilsons-MacBook-Air.local>
2022-12-09 22:29:23 -0800

    dsf

--- setree/Set.cpp
+++ setree/Set.cpp
@@ -195,7 +195,7 @@ void checkLeaf(const std::string& value, Node* currNode, Node* prevNode){
 }
 */
 size_t Set::remove (const std::string& value){
-    size_t amountRemoved = 0;
+    size_t amountRemoved = 1;
     delete mRoot;
     mRoot = nullptr;
     return amountRemoved;
